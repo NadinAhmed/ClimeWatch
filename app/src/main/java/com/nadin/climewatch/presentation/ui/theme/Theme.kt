884b8b98811590cbd25f3c6dd8b4a57a4fe21aef
Nadin Ahmed <nadinahmed316@gmail.com>
2026-03-03 21:13:49 +0200

    enhancing the bottomNavBar UI

--- app/src/main/java/com/nadin/climewatch/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/nadin/climewatch/presentation/ui/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.nadin.climewatch.R
 
 private val AppColorScheme = lightColorScheme(
@@ -46,7 +47,9 @@ fun ClimeWatchTheme(
             Image(
                 painter = painterResource(id = R.drawable.background),
                 contentDescription = null,
-                modifier = Modifier.fillMaxSize().alpha(0.1f),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(0.2f),
                 contentScale = ContentScale.Crop
             )
             content()
